bc457968c0332ef7c720e7b0092d3593ed489117
adarshcode007 <adarsh19pandey@gmail.com>
2025-09-08 06:38:04 +0530

    add commands

--- index.js
+++ index.js
@@ -15,9 +15,22 @@ client.on("messageCreate", (message) => {
   //   console.log(message.content);
 
   if (message.author.bot) return;
+
+  if (message.content.startsWith("create")) {
+    const url = message.content.split("create")[1];
+    return message.reply({
+      content: "Generating Short ID for " + url,
+    });
+  }
+
   message.reply({
     content: "Hi from Bot",
   });
+});
+
+client.on("interactionCreate", (interaction) => {
+  console.log(interaction);
+  interaction.reply("Pong!!");
 });
 
 client.login(process.env.token);
