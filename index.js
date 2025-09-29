686b6b48c7ccc3ffd8419b23a8751e04035231fc
adarshcode007 <adarsh19pandey@gmail.com>
2025-09-29 19:07:00 +0530

    Added Google gemini api to generate response

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import { Client, GatewayIntentBits } from "discord.js";
 import dotenv from "dotenv";
 dotenv.config();
 
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -10,22 +14,35 @@ const client = new Client({
   ],
 });
 
-client.on("messageCreate", (message) => {
-  //   console.log(message);
-  //   console.log(message.content);
+client.on("messageCreate", async (message) => {
+  // console.log(message);
+  // console.log(message.content);
 
   if (message.author.bot) return;
 
-  if (message.content.startsWith("create")) {
-    const url = message.content.split("create")[1];
-    return message.reply({
-      content: "Generating Short ID for " + url,
-    });
-  }
+  try {
+    if (message.content.startsWith("reply")) {
+      const prompt = message.content.split("reply")[1];
+      console.log(prompt);
 
-  message.reply({
-    content: "Hi from Bot",
-  });
+      const response = await ai.models.generateContent({
+        model: "gemini-2.5-flash",
+        contents: `Reply briefly and clearly: ${prompt}`,
+      });
+      const text = response.text;
+
+      return message.reply({
+        content: text || "Couldn't generate a response",
+      });
+    }
+
+    message.reply({
+      content: "Hi from Bot",
+    });
+  } catch (error) {
+    console.log(error.message);
+    return message.reply({ content: "Something went wrong!" });
+  }
 });
 
 client.on("interactionCreate", (interaction) => {
